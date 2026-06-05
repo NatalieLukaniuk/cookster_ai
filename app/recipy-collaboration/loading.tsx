cc5fab714f24e8850e133738dbf79ccc25e86b47
Natalie Klid <nklid@deloittece.com>
2026-06-05 09:29:51 +0300

    improved loading page

--- app/recipy-collaboration/loading.tsx
+++ app/recipy-collaboration/loading.tsx
@@ -1,8 +1,11 @@
+import { Spinner } from "@/components/ui/spinner";
+
 export default function Loading() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1 className="text-2xl font-bold">Loading...</h1>
+      <main className="flex flex-1 w-full flex-col items-center justify-center py-32 px-16 gap-3.5 bg-white dark:bg-black sm:items-start">
+        <h1 className="text-2xl font-bold">Loading the page, please give us a few seconds</h1>
+        <Spinner/>
       </main>
     </div>
   );
